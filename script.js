5806fcffb64ad83398974e5ee64d52697d24fdd1
tiagofernandess <tiagofernandes0095@gmail.com>
2024-08-03 14:25:57 -0300

    alteração no js

--- script.js
+++ script.js
@@ -3,7 +3,13 @@ function generateNumber(){
     const min = Math.ceil(document.querySelector(".input-min").value)
     const max = Math.floor(document.querySelector(".input-max").value)
 
-    const result = Math.floor(Math.random() * (max - min + 1)) + min;
+    if (min >= max) {
+        alert("O Valor minimo tem que ser MENOR que o valor máximo")
+    } else {
+        const result = Math.floor(Math.random() * (max - min + 1)) + min;
 
     alert(result)
-} 
+    } 
+}
+
+    
