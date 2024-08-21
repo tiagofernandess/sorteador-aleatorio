e92ee5d67c6df0da76dd5d84248120d8ba990874
tiagofernandess <tiagofernandes0095@gmail.com>
2024-08-21 15:09:49 -0300

    melhorias

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-function generateNumber(){
-    
+function generateNumber() {
+
     const min = Math.ceil(document.querySelector(".input-min").value)
     const max = Math.floor(document.querySelector(".input-max").value)
 
     const result = Math.floor(Math.random() * (max - min + 1)) + min;
 
-    alert(result)
-} 
+    const receiveNumber = document.querySelector(".receive-number")
+
+
+    receiveNumber.innerHTML = `Número Sorteado: ${result}`
+    receiveNumber.style.display = "block"
+}
