90c9560b053fea18dc39ecbf773b1b411509b877
tiagofernandess <tiagofernandes0095@gmail.com>
2024-11-02 15:42:28 -0300

    treinando js

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-function generateNumber() {
-
+function numberGeneration() {
     const min = Math.ceil(document.querySelector(".input-min").value)
     const max = Math.floor(document.querySelector(".input-max").value)
+    const result = Math.floor(Math.random() * (max - min + 1)) + min;
+    const receive = document.querySelector("#receive-number")
 
-    if (min >= max) {
-        alert("O valor mínimo tem que ser MENOR que o valor máximo.")
-    } else {
-        const result = Math.floor(Math.random() * (max - min + 1)) + min;
-
-        const receiveNumber = document.querySelector(".receive-number")
-
-        receiveNumber.innerHTML = `Número Sorteado: ${result}`
-        receiveNumber.style.display = "block"
+    if (min == max) {
+        alert("O numeros São Iguais. Por favor Insira numero Validos!")
     }
 
+    if (min > max) {
+        alert("O valor minimo não pode ser maior que o Maximo!")
+    }
 
-
+    if (min < max) {
+        receive.innerHTML = `Número Sorteado: ${result}`;
+        receive.style.display = "block"
+    }
 }
